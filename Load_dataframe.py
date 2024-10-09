0f0ef8f8efc1ccce0f22c0c26ffdad0f10424924
James Cobley <j_cobley@yahoo.com>
2024-10-09 14:32:29 +0100

    Update Load_dataframe.py
    This script uses a colab widget to enable a selected Excel file to be uploaded into the content folder before the column names are printed. 
    
    To run the script install the requirements.txt or use "!pip install xlsxwriter"
    
    The code can be modified and customised as desired.

--- Load_dataframe.py
+++ Load_dataframe.py
@@ -1,11 +1,18 @@
+from google.colab import files
 import pandas as pd
 
-# Load your protein group report file
-file_path = '/content/Report_HTT_James.xlsx'
+# Use the upload widget in colab to upload a file
+uploaded = files.upload()
+
+# Get the uploaded file path
+file_path = next(iter(uploaded))
+
+# Load the Excel file
 xls = pd.ExcelFile(file_path)
 
 # Load the first sheet into a DataFrame
-df = pd.read_excel(xls, sheet_name='Sheet 1')
+df = pd.read_excel(xls, sheet_name=xls.sheet_names[0])
 
 # Print the column names
+print("Column names in the selected file")
 print(df.columns)
